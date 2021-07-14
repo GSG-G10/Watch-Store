0f3174d8e0e349450abd03a5e40ff6e84f8d8671
umar <umars.salah@gmail.com>
2021-07-14 16:52:33 +0300

    Restore table and edit DOM Relates #35

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,24 +1,34 @@
+const IDfiled = document.getElementById("ProductIDField");
+const Namefiled = document.getElementById("ProductNameField");
+const IMGfiled = document.getElementById("ProductImgField");
+const PRICEfiled = document.getElementById("ProductPriceField");
+const DetailsID = document.getElementById("productID");
+const DetailsName = document.getElementById("productName");
+const DetailsIMG = document.getElementById("productURL");
+const DetailsPRICE = document.getElementById("productPrice");
+const myTable = document.getElementById("ProTable");
+
 const openForm = () => {
-  document.getElementById("myForm").style.display = "block";
+    document.getElementById("myForm").style.display = "block";
 };
 
 const closeForm = () => {
-  document.getElementById("myForm").style.display = "none";
+    document.getElementById("myForm").style.display = "none";
 };
 //---------------------Product properities------------------
 const getProductName = () => {
-  return document.getElementById("productName").value;
+    return DetailsName.value;
 };
 const getProductID = () => {
-  return document.getElementById("productID").value;
+    return IDfiled.value;
 };
 const getProductURL = () => {
-  return document.getElementById("productURL").value;
+    return IMGfiled.value;
 };
 const getProductPrice = () => {
-  return document.getElementById("productPrice").value;
+    return PRICEfiled.value;
 };
 //Go to productDetail function in the logic.js
 const AddProductBtn = document
-  .getElementById("AddProductBtn")
-  .addEventListener("click", ProductDetail);
+    .getElementById("AddProductBtn")
+    .addEventListener("click", ProductDetail);
