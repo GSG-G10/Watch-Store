3b621d6b4461982a5b87b49d7d03b36ab24137ea
muath-khatib <mo3az.khatib@hotmail.com>
2021-07-14 18:04:21 +0300

    fix-dom-not-good Relates #31

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,5 +1,4 @@
 //Add the productDetail into the local storage.
-// const lastProductIdAdded = ((productId) => productId);
 const ProductDetail = () => {
   let productObj = {
     name: getProductName(),
@@ -13,12 +12,10 @@ const ProductDetail = () => {
 };
 // Set Local Storage Keys In Array
 const localKeys = () => Object.keys(localStorage).sort((a,b) => a-b);
-// ["3","3","3","3","3","3",]
 
 // Mapping And Create new Array Of Object
 const productStored = localKeys().map((e) => {
   const elementObj = localStorage.getItem(parseInt(e));
   return JSON.parse(elementObj);
 });
-// console.log(productStored)
 
